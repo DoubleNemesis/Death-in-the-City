b1fcf53976112c149bd1973dac06d9261cb6ef39
TomChant <chantaspell@gmail.com>
2021-07-03 22:35:00 +0100

    Crime scene component

--- totb/src/pages/CrimeScene.js
+++ totb/src/pages/CrimeScene.js
@@ -8,17 +8,8 @@ function CrimeScene(){
 
     return(
         <>
-        <Title>{clientTitle}</Title>
-            <PageContainer>
-                {clientText}
-                <NextPageButton destination="office">Go to office</NextPageButton>
-                In your office you will find three things:
-                <ul>
-                    <li>a map - you can use this to navigate the city</li>
-                    <li>an ideas board - you can see the suspects you have spoken to and arrange your ideas</li>
-                    <li>A notebook - you can view your notes and get other ideas and suggestions</li>
-                    </ul>
-            </PageContainer>
+{/* {images} */}
+
         </>
     )
 }
