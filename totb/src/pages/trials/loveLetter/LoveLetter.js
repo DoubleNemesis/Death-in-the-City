682670a9e2170c1dbda5813b4da6adf482171832
TomChant <chantaspell@gmail.com>
2021-07-12 15:56:16 +0100

    Updated logic for loveletter to stop repeat letter selection

--- totb/src/pages/trials/loveLetter/LoveLetter.js
+++ totb/src/pages/trials/loveLetter/LoveLetter.js
@@ -1,14 +1,16 @@
-import { useContext, useRef, useState } from 'react'
+import { useContext, useEffect, useRef, useState } from 'react'
 import PageContainer from '../../../containers/PageContainer'
 import Title from '../../../generalComponents/Title'
 //import { history, useHistory } from 'react-router-dom'
 import { SpeechBubbleLeft } from '../../witness/witnessComponents/Questions'
 import { Instructions, Conversation, WitnessImage, TaskBox } from '../../witness/witnessComponents/Layout'
 import { LoveLetterMainContainer, LoveLetterElems, LoveLetterSymbolsContainer, LoveLetterLettersContainer, LoveLetterSpace, LoveLetterSymbolElems } from './LoveLetterComponents'
+import {StyledModal, ToggleContainer, ToggleTaskInfo, QuestionOption} from '../../../generalComponents/InfoModal'
 import ProfilePic from '../../../images/chaymadz.jpg'
 import GameContext from '../../../context/GameContext'
 import Draggable from 'react-draggable';
 import { loveLetterData } from '../../../data/lessonData'
+import NextPageButton from '../../../generalComponents/NextPageButton'
 
 
 
@@ -20,6 +22,7 @@ function LoveLetter() {
     const [selectedSymbol, setSelectedSymbol] = useState('')
     const [usedLetters, setUsedLetters] = useState([])
     const [successMessage, setSuccessMessage] = useState('')
+    const [isInstructionsModalDisplayed, setIsInstructionsModalDisplayed] = useState(true)
 
     const nodeRef = useRef(null);
     const { instructions, loveLetterCode } = loveLetterData
@@ -27,8 +30,10 @@ function LoveLetter() {
     const secretMessageArray = secretMessage.toLowerCase().split('');
 
 
-    function handleLetterClick(e) {
+    function handleLetterClick(e) { 
         setSelectedLetter(e.target.id)
+        
+        //make letter unclickable
     }
 
     function handleSymbolClick(e) {
@@ -58,13 +63,39 @@ function LoveLetter() {
         }
     }
 
-    const LoveLetterLetters = loveLetterData['letters'].map((item, index) => <LoveLetterElems draggable="true" color={usedLetters.indexOf(item[0]) > -1 ? 'transparent' : item[0] === selectedLetter ? 'red' : 'beige'} onClick={handleLetterClick} id={item[0]}>{item[0]}</LoveLetterElems>)
+    useEffect(()=>{
+        console.log(usedLetters);
+        usedLetters.map((item=>{
+            document.getElementById(item).classList.add('unClickable')
+        }))
+        
+    },[usedLetters])
+
+
+    const LoveLetterLetters = loveLetterData['letters'].map((item, index) => <LoveLetterElems color={usedLetters.indexOf(item[0]) > -1 ? 'transparent' : item[0] === selectedLetter ? 'red' : 'beige'} onClick={handleLetterClick} id={item[0]}>{item[0]}</LoveLetterElems>)
     const LoveLetterCode = secretMessageArray.map((item, index) => {
         const targetSymbol = typeof loveLetterData['symbols'][loveLetterData['letters'].indexOf(item)] === 'object' ? String.fromCharCode(loveLetterData['symbols'][loveLetterData['letters'].indexOf(item)][1]) : null
         const symbolTiles = 'symbolTiles' 
 
         return (
             <>
+                        <StyledModal display={isInstructionsModalDisplayed ? 'block' : 'none'}>
+                <h2>Task: Dialogue</h2>
+                <ul>
+                    <li> Read the text in the speech bubble.</li>
+                    <li> Choose a reply from the list to continue the conversation.</li>
+                    <li> Only one question is grammatically correct. </li>
+                    <li> If you choose the wrong answer, you lose a point.</li>
+                </ul>
+                <ToggleContainer>
+                <ToggleTaskInfo
+                    onClick={() => {
+                        setIsInstructionsModalDisplayed(!isInstructionsModalDisplayed)
+                        }}>
+                    Start
+                </ToggleTaskInfo>
+                </ToggleContainer>
+            </StyledModal>
                 { targetSymbol ?
                     <LoveLetterSymbolElems className={`${targetSymbol.charCodeAt(0)} symbolsClass`} onClick={handleSymbolClick} color={targetSymbol.charCodeAt(0) === parseInt(selectedSymbol) ? 'red' : 'beige'}>{targetSymbol}</LoveLetterSymbolElems> :
                     <LoveLetterSpace />
@@ -80,9 +111,9 @@ function LoveLetter() {
                     <LoveLetterMainContainer>
                     <LoveLetterSymbolsContainer>
                         {LoveLetterCode}
-                        {successMessage}
+                        
                     </LoveLetterSymbolsContainer>
-
+                    {successMessage}
                     <LoveLetterLettersContainer>
 
                         {LoveLetterLetters}
--- totb/src/pages/trials/loveLetter/LoveLetterComponents.js
+++ totb/src/pages/trials/loveLetter/LoveLetterComponents.js
@@ -10,7 +10,7 @@ align-items: center;
 export const LoveLetterSymbolsContainer = styled.div`
 display: flex;
 flex-wrap: wrap;
-width: 320px;
+width: 325px;
 border: 1px solid red;
 margin-top: 1em;
 padding: .1em;
@@ -20,6 +20,11 @@ width: 100%;
 display: flex;
 flex-wrap: wrap;
 margin-top: 2em;
+
+.unClickable{
+    pointer-events: none;
+    background-color: black;
+}
 `
 
 export const LoveLetterElems = styled.span`
@@ -31,6 +36,9 @@ width: 20px;
 padding: .3em .5em;
 margin: .2em;
 border-radius: .2em;
+
+
+
 `
 export const LoveLetterSymbolElems = styled.span`
 display: flex;
