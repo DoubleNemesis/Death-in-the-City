6be50ff4212d5123582849f6172187a9b6621259
TomChant <chantaspell@gmail.com>
2021-08-21 17:12:19 +0100

    clear up unneeded files

--- not needed/officeComponents/NoteItem.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components'
-
-const StyledNote = styled.div`
-color: red;
-font-family: cursive;
-`
-
-export const NoteItem = (props)=><><StyledNote >{props.text}</StyledNote></>
